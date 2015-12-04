a2bb4f2aefff03cfc20316adb94d42a65e9cb8aa
ch.yang <ch.yang@navercorp.com>
2015-12-04 11:00:08 +0900

    CafeSDK for android start

---
+++ README.md
@@ -0,0 +1,39 @@
+# Andrid용 Cafe SDK 
+
+> 본 저장소는 Cafe SDK를 사용하기위한 library 및 관련 파일들을 포함하고 있습니다.
+
+Cafe SDK에 대하여 자세히 알고싶으시면 [네이버 CafeSDK 공식카페](http://cafe.naver.com/navercafesdk)를 참조해 주세요.
+
+
+
+
+v.1.0.0
+-------------
+ NaverCafeSDK 오픈
+
+
+폴더 구조
+-------------
+
+file      | 설명 		
+---			| ---		
+guide/			| 가이드 문서 및 CafeSDK 설명
+lib/		 	| Android용 CafeSDK 라이브러리 파일
+sample/		| eclipse 및 android studio 용 sample project
+LICENSE     | 라이선스 파일
+README.md   | readme 파일
+
+
+의존 라이브러리
+-------------
+
+1. [네이버 아이디 로그인](https://nid.naver.com/devcenter/docs.nhn?menu=Android)
+2. android support library v13
+3. [naver volleyer](http://mvnrepository.com/artifact/com.navercorp.volleyextensions/volleyer)   (2.0.1 사용 중)
+4. [volley](http://mvnrepository.com/artifact/com.mcxiaoke.volley/library/) (1.0.2사용 중)
+5. [google gson](http://mvnrepository.com/artifact/com.google.code.gson/gson)  (2.3.1 사용 중)
+6. [glide](http://mvnrepository.com/artifact/com.github.bumptech.glide/glide)  (3.6.1 사용 중)
+7. [otto](http://mvnrepository.com/artifact/com.squareup/otto)  (1.3.8 사용 중)
+8. [VierwPagerIndicator](http://mvnrepository.com/artifact/com.viewpagerindicator/library) (2.4.1 사용 중) 
+<br>**ViewPagerIndicator의 경우 jar가 아닌 라이브러리 형태로 배포되고 있습니다. aar or apklib or zip으로 받아 개발툴에 맞는 라이브러리를 사용하시면 됩니다.
+<br>**aar (https://bintray.com/populov/maven/com.viewpagerindicator:library)
---
+++ sample/navercafesdk-sample-android-studio/app/src/main/java/com/naver/glink/sample/MainActivity.java
@@ -0,0 +1,99 @@
+package com.naver.glink.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.naver.glink.android.sdk.Glink;
+
+public class MainActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    /**
+     * "네아로 개발자 센터"에서 받은 정보로 SDK를 초기화 합니다.
+     * Glink의 다른 메소드를 호출하기 전에 반드시 초기화를 먼저해야 합니다.
+     * 개발자 센터 주소: https://nid.naver.com/devcenter/main.nhn
+     */
+    final int cafeId = 28290504;
+    final String clientId = "197CymaStozo7X5r2qR5";
+    final String clientSecret = "evCgKH1kJL";
+    Glink.init(clientId, clientSecret, cafeId);
+
+    // 게임 아이디와 카페 아이디를 매핑합니다.
+    // 프로필 화면에서 매핑된 게임 아이디를 확인할 수 있습니다.
+    Glink.setGameUserId(this, "id123", "게임 ID");
+
+    // 앱스킴 터치 리스너 설정.
+    Glink.setOnClickAppSchemeBannerListener(new Glink.OnClickAppSchemeBannerListener() {
+      @Override public void onClickAppSchemeBanner(String appScheme) {
+        // 카페 관리에서 설정한 appScheme 문자열을 SDK에서 넘겨줍니다.
+        // 각 appScheme 처리를 이곳에서 하시면 됩니다.
+        Toast.makeText(MainActivity.this, appScheme, Toast.LENGTH_LONG).show();
+      }
+    });
+
+    findViewById(R.id.home_button).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        // 홈화면으로 시작합니다.
+        Glink.startHome(MainActivity.this);
+      }
+    });
+
+    findViewById(R.id.notice_button).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        // 공지화면으로 시작합니다.
+        Glink.startNotice(MainActivity.this);
+      }
+    });
+
+    findViewById(R.id.event_button).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        // 이벤트화면으로 시작합니다.
+        Glink.startEvent(MainActivity.this);
+      }
+    });
+
+    findViewById(R.id.menu_button).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        // 메뉴화면으로 시작합니다.
+        Glink.startMenu(MainActivity.this);
+      }
+    });
+
+    findViewById(R.id.profile_button).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        // 프로필화면으로 시작합니다.
+        Glink.startProfile(MainActivity.this);
+      }
+    });
+
+    findViewById(R.id.write_button1).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        int menuId = 4; // 0이면 메뉴를 선택하지 않는다.
+        String text = "기본 제목, 본문을 넣어서 글쓰기 화면을 시작합니다.";
+        Glink.startWrite(MainActivity.this, menuId, "subject", text);
+      }
+    });
+
+    findViewById(R.id.write_button2).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        int menuId = 4; // 0이면 메뉴를 선택하지 않는다.
+        String text = "기본 제목, 본문, 이미지를 넣어서 글쓰기 화면을 시작합니다.\n이미지는 uri 형태로 넣어주시면 됩니다.";
+        String imageUri = "http://cafeimgs.naver.net/glink/img/gl_img_copyright.png";
+        Glink.startImageWrite(MainActivity.this, menuId, "subject", text, imageUri);
+      }
+    });
+
+    findViewById(R.id.write_button3).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        int menuId = 4; // 0이면 메뉴를 선택하지 않는다.
+        String text = "기본 제목, 본문, 비디오를 넣어서 글쓰기 화면을 시작합니다.\n비디오는 uri 형태로 넣어주시면 됩니다.";
+        String videoUri = "file://your_video_path";
+        Glink.startVideoWrite(MainActivity.this, menuId, "subject", text, videoUri);
+      }
+    });
+  }
+}
